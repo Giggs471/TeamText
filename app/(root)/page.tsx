7676a2d3dfc163f7eba81066e88e1b7f0ab0c65e
Giggs471 <blitzwizard45@gmail.com>
2024-08-06 20:09:49 +0500

    added files

---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Button} from '@/components/ui/button'
+
+function Home() {
+  return (
+    <div>
+      <Button>click me</Button>
+    </div>
+  )
+}
+
+export default Home
---
+++ app/provider.tsx
@@ -0,0 +1,17 @@
+'use client'
+
+import Loader from '@/components/loader'
+import {ClientSideSuspense, LiveblocksProvider} from '@liveblocks/react/suspense'
+import React from 'react'
+
+const provider = ({children}: {children: React.ReactNode}) => {
+  return (
+    <LiveblocksProvider authEndpoint="/api/liveblocks-auth">
+        <ClientSideSuspense fallback={<Loader/>}>
+          {children}
+        </ClientSideSuspense>
+    </LiveblocksProvider>
+  )
+}
+
+export default provider
